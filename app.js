4a5352909de4782e00cc31606b286c2de4c96a4a
Michelle Stermitz <stermitzm@gmail.com>
2020-06-24 20:21:35 -0700

    fixed eslint issues and touched up CSS

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 const pinkButton = document.getElementById('pinkbutton');
-console.log('Pink')
+console.log('Pink');
 pinkButton.addEventListener('click', () => { 
     const PinkColorTop = document.getElementById('headerbackground');
     PinkColorTop.style.background = 'pink';
     const PinkColorBottom = document.getElementById('footerbackground');
     PinkColorBottom.style.background = 'pink';
-})
+});
 
 const greenButton = document.getElementById('greenbutton');
-console.log('Green')
+console.log('Green');
 greenButton.addEventListener('click', () => { 
     const GreenColorTop = document.getElementById('headerbackground');
     GreenColorTop.style.background = 'lightgreen';
     const GreenColorBottom = document.getElementById('footerbackground');
     GreenColorBottom.style.background = 'lightgreen';
-})
+});
 
 const blueButton = document.getElementById('bluebutton');
-console.log('Blue')
+console.log('Blue');
 blueButton.addEventListener('click', () => { 
     const blueColorTop = document.getElementById('headerbackground');
     blueColorTop.style.background = 'lightblue';
     const blueColorBottom = document.getElementById('footerbackground');
     blueColorBottom.style.background = 'lightblue';
-})
+});
 
 const nameInput = document.getElementById('NameInput');
 console.log('NameInput');
@@ -34,5 +34,5 @@ nameButton.addEventListener('click', () => {
     const changeName = document.getElementById('namedisplay');
     const value = nameInput.value;
     changeName.textContent = value;
-} )
+});
 
